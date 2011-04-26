0607ad36a137a108d3af4d12729e1fc3a91e0046
Ashley Towns <ashleyis@me.com>
2011-04-26 18:53:03 +1000

    optimisations

--- 4scrape.js
+++ 4scrape.js
@@ -1,21 +1,24 @@
 #!/usr/local/bin/node
 // Internal deps
-var board   = require("./board");
+var board     = require("./board");
 // External deps
-var _       = require("underscore");
-var sys     = require("sys");
-var mongo   = require("mongodb")
+var _         = require("underscore");
+var sys       = require("sys");
+var mongo     = require("mongodb");
+var gridstore = mongo.GridStore;
 
-var boards = ["/b/", "/soc/"];
+var boards = ["/b/"];
 var client = new mongo.Db('4scrape', new mongo.Server('127.0.0.1', 27017, {}));
 
 _.each(boards, function (chan) {
   // updates boards every 30 seconds
-  board.Grab(chan, 30, function (posts) {
+  board.Grab(chan, 60, function (posts) {
+    sys.puts(sys.inspect(posts));
     inserta = function(err, col) {
-      col.update({threadid: posts.threadid}, {$set: {posts: posts.posts}}, {upsert: true, safe: true}, function (err) {
+      // Can't save images, convert to gridstore here, hold refs in .image
+      col.update({threadid: posts.threadid}, {$set: {posts: posts}}, {upsert: true, safe: true}, function (err) {
         if (err) sys.puts("Error upserting: "+posts.threadid);
-        else sys.puts("Updated: "posts.threadid);
+        else sys.puts("Updated: "+posts.threadid);
       }); 
     };
     client.open(function(err, p_client) { client.collection('newposts', inserta); });
